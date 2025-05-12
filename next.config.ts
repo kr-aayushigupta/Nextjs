b6b25badcc0f09515bfe254407025811e9c6d42e
kr-aayushigupta <aayushi.g@knackroot.com>
2025-05-12 18:15:41 +0530

    nextjs

--- next.config.ts
+++ next.config.ts
@@ -4,7 +4,7 @@ const nextConfig: NextConfig = {
   reactStrictMode: true,
   eslint: { ignoreDuringBuilds: true },
   typescript: { ignoreBuildErrors: true },
-  /* config options here */
+  
 };
 
 export default nextConfig;
